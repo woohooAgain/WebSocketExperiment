647c1722592f4ea4e7e6f8cae1f7fe68ae7296ef
ikulikov <ilia.s.kulikov@gmail.com>
2018-10-02 01:06:25 +0300

    Add project files.

---
+++ WebSocketExperiment/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace WebSocketExperiment
+{
+    public class Program
+    {
+        public static int OpenedConnections { get; set; }
+        public static List<Guid> Clients { get; set; }
+        public static void Main(string[] args)
+        {
+            var OpenedConnections = 0;
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+    }
+}
